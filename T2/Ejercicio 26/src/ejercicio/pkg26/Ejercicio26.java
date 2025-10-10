6f759638dadc6a97fc103567f6676919778ed595
1DAW Luis Daniel Triminio Coello <ldtriminioc01@iesvjp.es>
2025-10-10 19:03:08 +0200

    Ejercicio Obligatorio

--- T2/Ejercicio 26/src/ejercicio/pkg26/Ejercicio26.java
+++ T2/Ejercicio 26/src/ejercicio/pkg26/Ejercicio26.java
@@ -4,7 +4,8 @@
  */
 package ejercicio.pkg26;
 
-import java.util.Scanner;
+import java.util.Scanner;//Scanner para que el usuario introduzca los datos 
+
 /**
  *
  * @author admin
@@ -15,20 +16,24 @@ public class Ejercicio26 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //programa en el que le pidas al usuario un número de 4 cifras y
+        //muestre por pantalla cada una de las cifras que lo forman.
+        
+        //Atributo
         int numero, primeraCifra, segundaCifra, terceraCifra, cuartaCifra;
-        
-        Scanner entrada = new Scanner (System.in);
-        
-        System.out.println("Introduzca un numero de 4 cifras: ");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Introduzca un numero de 4 cifras: ");//Sout para pedir las cifras 
         numero = entrada.nextInt();
-        primeraCifra = numero / 1000 ;
-        System.out.println("La primera cifra es: " + primeraCifra);
-        segundaCifra = numero % 1000 / 100 ;
-        System.out.println("La segunda cifra es: " + segundaCifra);
+        primeraCifra = numero / 1000;
+        System.out.println("La primera cifra es: " + primeraCifra);//Sout para pedir la primera  cifra
+        segundaCifra = numero % 1000 / 100;
+        System.out.println("La segunda cifra es: " + segundaCifra);//Sout para pedir la segunda  cifra
         terceraCifra = numero % 100 / 10;
-        System.out.println("La tercera cifra es: " + terceraCifra);
+        System.out.println("La tercera cifra es: " + terceraCifra);//Sout para pedir tercera cifras 
         cuartaCifra = numero % 10;
-        System.out.println("La cuarta cifra es: " + cuartaCifra);
+        System.out.println("La cuarta cifra es: " + cuartaCifra);//Sout para pedir cuarta cifras 
     }
-    
+
 }
---
+++ T2/Ejercicio 32/src/ejercicio/pkg32/Ejercicio32.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio.pkg32;
+
+import java.util.Scanner;//Scanner para que el usuario introduzca los datos 
+
+/**
+ *
+ * @author danielcoello
+ */
+public class Ejercicio32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // programa que dado un importe en euros nos indique número
+        //óptimo de billetes de 50, 20, 10 y 5, así como la cantidad sobrante
+        //en monedas de 2 y de 1 euro.
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Por favor, indique una cantidad de dinero: ");//Indico al usuario que meta los datos.
+        int dineroTotal = entrada.nextInt();
+
+        int billetesDe50 = dineroTotal / 50;
+        dineroTotal = dineroTotal % 50;
+
+        int billetesDe20 = dineroTotal / 20;
+        dineroTotal = dineroTotal % 20;
+
+        int billetesDe10 = dineroTotal / 10;
+        dineroTotal = dineroTotal % 10;
+
+        int billetesDe5 = dineroTotal / 5;
+        dineroTotal = dineroTotal % 5;
+
+        int monedasDe2 = dineroTotal / 2;
+        dineroTotal = dineroTotal % 2;
+
+        int monedasDe1 = dineroTotal; // lo que queda son monedas de 1 euro
+
+        System.out.println("La cantidad se descompone en:");
+        System.out.println(billetesDe50 + " billetes de 50 euros");
+        System.out.println(billetesDe20 + " billetes de 20 euros");
+        System.out.println(billetesDe10 + " billetes de 10 euros");
+        System.out.println(billetesDe5 + " billetes de 5 euros");
+        System.out.println(monedasDe2 + " monedas de 2 euros");
+        System.out.println(monedasDe1 + " monedas de 1 euro");
+    }
+}
